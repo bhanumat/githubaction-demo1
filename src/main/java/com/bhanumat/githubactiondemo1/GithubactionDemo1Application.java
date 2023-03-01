37134dc7b4cb304126ee3f246e82162a1aea1e77
bhanumaw <bhanumaw@ais.co.th>
2023-03-01 15:26:08 +0700

    Initialize Springboot from initializer

---
+++ src/main/java/com/bhanumat/githubactiondemo1/GithubactionDemo1Application.java
@@ -0,0 +1,13 @@
+package com.bhanumat.githubactiondemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhanumat/githubactiondemo1/GithubactionDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhanumat.githubactiondemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
