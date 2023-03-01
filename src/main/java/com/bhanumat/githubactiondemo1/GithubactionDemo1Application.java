bf77ff48ac5732eccaf7e0ee5088c8667c0c05f8
bhanumaw <bhanumaw@ais.co.th>
2023-03-01 16:58:31 +0700

    Add print out to console

--- src/main/java/com/bhanumat/githubactiondemo1/GithubactionDemo1Application.java
+++ src/main/java/com/bhanumat/githubactiondemo1/GithubactionDemo1Application.java
@@ -8,6 +8,7 @@ public class GithubactionDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubactionDemo1Application.class, args);
+		System.out.println("Hello World");
 	}
 
 }
